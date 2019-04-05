afa00377a33b9ef090bd9e028b94c24d61bfe4ea
金井龍一 <kanairyuuichi@eduroam-int-dhcp-97-163-36.ucl.ac.uk>
2019-04-05 14:49:55 +0100

    first add

---
+++ 1_1answer.cpp
@@ -0,0 +1,11 @@
+#include<isotream>
+int main()
+{
+    int a, b, c;
+    std::cin>>a;
+    std::cin>>b>>c;
+    std::string s;
+    std::cin>> s;
+    std::count<<(a+b+c) <<" "<<s <<std::end;
+    return 0;
+}
